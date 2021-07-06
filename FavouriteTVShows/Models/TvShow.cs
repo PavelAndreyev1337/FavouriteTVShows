2e543b0215b3b7f78f79a4d8960a02c8f3562038
PavelAndreyev1337 <pavel.andreyev.98@gmail.com>
2021-07-06 13:24:32 +0300

    created models

---
+++ FavouriteTVShows/Models/TvShow.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FavouriteTVShows.Models
+{
+    public class TvShow
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public Genre Genre { get; set; }
+
+        public decimal Rating { get; set; }
+
+        public string ImdUrl { get; set; }
+
+    }
+}
