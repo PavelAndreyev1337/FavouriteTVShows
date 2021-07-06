5a191b80d17347620cb18aba1f2ac56de5294ab8
PavelAndreyev1337 <pavel.andreyev.98@gmail.com>
2021-07-06 14:30:20 +0300

    added image url migration

--- FavouriteTVShows/Migrations/20210706112916_ImageUrl.Designer.cs
+++ FavouriteTVShows/Migrations/20210706112916_ImageUrl.Designer.cs
@@ -3,14 +3,16 @@ using FavouriteTVShows.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FavouriteTVShows.Migrations
 {
     [DbContext(typeof(TvShowsContext))]
-    partial class TvShowsContextModelSnapshot : ModelSnapshot
+    [Migration("20210706112916_ImageUrl")]
+    partial class ImageUrl
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ FavouriteTVShows/Migrations/20210706112916_ImageUrl.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FavouriteTVShows.Migrations
+{
+    public partial class ImageUrl : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "TvShow",
+                maxLength: 60,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImdUrl",
+                table: "TvShow",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "TvShow",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "TvShow");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "TvShow",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 60);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImdUrl",
+                table: "TvShow",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
